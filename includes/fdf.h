61a81cea71c4dac4d312ce29e3b394c052ee94d1
Maxence Chanliat <mchanlia@f2r4s3.clusters.42paris.fr>
2025-08-20 12:43:16 +0200

    debut struct

--- includes/fdf.h
+++ includes/fdf.h
@@ -3,5 +3,17 @@
 
 #include "../Libft/My_libft.h"
 #include "../minilibx-linux/mlx.h"
+#include "math.h"
 
+typedef struct	s_mlx
+{
+	t_position tab[50];
+}				t_mlx;
+typedef struct	s_position
+{
+	int X_pos;
+	int Y_pos;
+	int Z_pos;
+	int	color;
+}				t_position;
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,5 +3,20 @@
 int	main(void)
 {
 	ft_printf(1, "hello world\n");
+	t_position **tab; // len tab 
 	return(0);
 }
+// tester les qrgs, tester l'extension du fichier, tester la validite de la map
+// ranger dans un tableau de structure contenant tout les infos (position couleur ...).
+
+// //• open, close, read, write,
+// malloc, free, perror,
+// strerror, exit.
+// • All functions of the math
+// library (-lm compiler option,
+// man 3 math on some operating
+// systems).
+// • All functions of the MiniLibX
+// library.
+// • ft_printf or any equivalent
+// YOU coded
